1a93ab63cbdf9db8093fe8abf543ed4327c0e721
gayah6996-del <gayah6996@gmail.com>
2025-09-13 03:36:21 +0300

    Add files via upload

--- Test-99Night.lua
+++ Test-99Night.lua
@@ -40,21 +40,21 @@ SpeedHack.Name = "SpeedHack"
 SpeedHack.Parent = Frame
 SpeedHack.BackgroundColor3 = Color3.fromRGB(76, 76, 76)
 SpeedHack.BorderColor3 = Color3.fromRGB(0, 0, 0)
-SpeedHack.Position = UDim2.new(0.003303082, 0, 0.0846896172, 0)
+SpeedHack.Position = UDim2.new(0.033303082, 0, 0.0846896172, 0)
 SpeedHack.Size = UDim2.new(0, 340, 0, 77)
 SpeedHack.Font = Enum.Font.GothamBold
 SpeedHack.Text = "Enable SpeedHack"
 SpeedHack.TextColor3 = Color3.fromRGB(255, 255, 255)
 SpeedHack.TextSize = 3.000
 SpeedHack.MouseButton1Down:connect(function()
-loadstring(game:HttpGet("https://pastebin.com/raw/a95RwWVu", true))()
+loadstring(game:HttpGet("https://pastefy.app/e2vnb32j/raw", true))()
 end)
 
 Noclip.Name = "NoClip"
 Noclip.Parent = Frame
 Noclip.BackgroundColor3 = Color3.fromRGB(76, 76, 76)
 Noclip.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Noclip.Position = UDim2.new(0.033303082, 0, 0.0846896172, 0)
+Noclip.Position = UDim2.new(0.033303082, 0, 0.570439577, 0)
 NoClip.Size = UDim2.new(0, 340, 0, 77)
 Noclip.Font = Enum.Font.GothamBold
 Noclip.Text = "Enable Noclip"
@@ -68,7 +68,7 @@ Close.Name = "Close"
 Close.Parent = Frame
 Close.BackgroundColor3 = Color3.fromRGB(76, 76, 76)
 Close.BorderColor3 = Color3.fromRGB(97, 97, 97)
-Close.Position = UDim2.new(0.033303082, 0, 0.570439577, 0)
+Close.Position = UDim2.new(0.033303082, 0, 0.36523556, 0)
 Close.Size = UDim2.new(0, 340, 0, 77)
 Close.Font = Enum.Font.GothamBold
 Close.Text = "Close GUI"
