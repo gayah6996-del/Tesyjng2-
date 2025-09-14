43864761917b0d5bd8be2ac23cd101a882206d24
gayah6996-del <gayah6996@gmail.com>
2025-09-14 23:40:16 +0300

    Add files via upload

--- Walkspeed_gui.lua
+++ Walkspeed_gui.lua
@@ -3,13 +3,19 @@ local mouse = player:GetMouse()local jumpEnabled = false
 local speedEnabled = false
 
 -- Создание меню
-local screenGui = Instance.new("ScreenGui", player.PlayerGui)screenGui.Name ="@SFXCL"local jumpButton = Instance.new("TextButton", screenGui)jumpButton.Size = UDim2.new(0, 200, 0, 50)jumpButton.Position = UDim2.new(0.5, -100, 0.5, -30)jumpButton.Text ="Toggle Infinity Jump"local speedButton = Instance.new("TextButton", screenGui)speedButton.Size = UDim2.new(0, 200, 0, 50)speedButton.Position = UDim2.new(0.5, -100, 0.5, 20)speedButton.Text ="Toggle Speed Hack"-- Функция бесконечного прыжка
+local screenGui = Instance.new("ScreenGui", player.PlayerGui)screenGui.Name ="@SFXCL"local jumpButton = Instance.new("TextButton", screenGui)jumpButton.Size = UDim2.new(0, 200, 0, 50)jumpButton.Position = UDim2.new(0.5, -100, 0.5, -30)jumpButton.Text ="Toggle Infinity Jump"jumpButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Красный цвет
+
+local speedButton = Instance.new("TextButton", screenGui)speedButton.Size = UDim2.new(0, 200, 0, 50)speedButton.Position = UDim2.new(0.5, -100, 0.5, 20)speedButton.Text ="Toggle Speed Hack"speedButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Красный цвет
+
+-- Функция бесконечного прыжка
 local function toggleJump()    jumpEnabled = not jumpEnabled
+    jumpButton.BackgroundColor3 = jumpEnabled and Color3.fromRGB(0, 255, 0) or Color3.fromRGB(255, 0, 0) -- Зеленый при включенном, красный при выключенном
     player.Character.Humanoid.JumpPower = jumpEnabled and 100 or 50
 end
 
 -- Функция ускорения
 local function toggleSpeed()    speedEnabled = not speedEnabled
+    speedButton.BackgroundColor3 = speedEnabled and Color3.fromRGB(0, 255, 0) or Color3.fromRGB(255, 0, 0) -- Зеленый при включенном, красный при выключенном
     player.Character.Humanoid.WalkSpeed = speedEnabled and 50 or 16
 end
 
