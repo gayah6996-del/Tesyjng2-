0f9c67a7e35040d4f70fb3400c4473fa2448f6f4
gayah6996-del <gayah6996@gmail.com>
2025-09-14 22:10:06 +0300

    Add files via upload

--- Walkspeed_gui.lua
+++ Walkspeed_gui.lua
@@ -1,5 +1,5 @@
 local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")local character = player.Character or player.CharacterAdded:Wait()local humanoid = character:WaitForChild("Humanoid")local infiniteJumpEnabled = false
+local playerGui = player:WaitForChild("PlayerGui")local character = player.Character or player.CharacterAdded:Wait()local humanoid = character:WaitForChild("Humanoid")local userInputService = game:GetService("UserInputService")local infiniteJumpEnabled = false
 local speedHackEnabled = false
 
 -- Создаем GUI
@@ -14,27 +14,21 @@ jumpButton.Size = UDim2.new(0, 200, 0, 50)jumpButton.Position = UDim2.new(0.5, -
 speedButton.Size = UDim2.new(0, 200, 0, 50)speedButton.Position = UDim2.new(0.5, -100, 0.5, 25)speedButton.Text ="Speed Hack: OFF"speedButton.Parent = screenGui
 
 -- Настройки кнопки закрытия
-closeButton.MouseButton1Click:Connect(function()
 closeButton.Size = UDim2.new(0, 100, 0, 50)closeButton.Position = UDim2.new(0.5, -50, 0.5, 100)closeButton.Text ="Close"closeButton.Parent = screenGui
 
 -- Привязываем функции к нажатию кнопок
 jumpButton.MouseButton1Click:Connect(function()    infiniteJumpEnabled = not infiniteJumpEnabled
-
-    if infiniteJumpEnabled then
-        jumpButton.Text ="Infinite Jump: ON"    else
-        jumpButton.Text ="Infinite Jump: OFF"    end
-end)speedButton.MouseButton1Click:Connect(function()    speedHackEnabled = not speedHackEnabled
-
+    jumpButton.Text = infiniteJumpEnabled and"Infinite Jump: ON" or"Infinite Jump: OFF"end)speedButton.MouseButton1Click:Connect(function()    speedHackEnabled = not speedHackEnabled
     if speedHackEnabled then
         speedButton.Text ="Speed Hack: ON"        humanoid.WalkSpeed = 100 -- Устанавливаем скорость
     else
         speedButton.Text ="Speed Hack: OFF"        humanoid.WalkSpeed = 16 -- Возвращаем скорость к норме
     end
 end)-- Обработчик нажатия пробела для бесконечного прыжка
-game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)    if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Space then
-        if infiniteJumpEnabled then
-            if humanoid:GetState() == Enum.HumanoidStateType.Freefall then
-                humanoid:ChangeState(Enum.HumanoidStateType.Jumping)            end
-        end
+userInputService.InputBegan:Connect(function(input, gameProcessed)    if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Space then
+        if infiniteJumpEnabled and humanoid:GetState() == Enum.HumanoidStateType.Freefall then
+            humanoid:ChangeState(Enum.HumanoidStateType.Jumping)        end
     end
 end)-- Закрытие GUI
+closeButton.MouseButton1Click:Connect(function()    screenGui:Destroy() -- Удаляем GUI
+end)
