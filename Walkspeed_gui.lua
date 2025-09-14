b639c64a0a181d5731e462a3e76ef455eb81318c
gayah6996-del <gayah6996@gmail.com>
2025-09-14 19:23:22 +0300

    Add files via upload

--- Walkspeed_gui.lua
+++ Walkspeed_gui.lua
@@ -5,6 +5,7 @@ local mouse = player:GetMouse()local infiniteJumpEnabled = false
 local screenGui = Instance.new("ScreenGui")local jumpButton = Instance.new("TextButton")screenGui.Parent = player:WaitForChild("PlayerGui")jumpButton.Size = UDim2.new(0, 200, 0, 50)jumpButton.Position = UDim2.new(0.5, -100, 0.5, -25)jumpButton.Text ="Infinite Jump"jumpButton.Parent = screenGui
 
 -- Функция для включения/выключения бесконечного прыжка
+jumpButton.MouseButton1Click:Connect(toggleInfiniteJump)
 local function toggleInfiniteJump()	infiniteJumpEnabled = not infiniteJumpEnabled
 	
 	if infiniteJumpEnabled then
@@ -14,8 +15,4 @@ local function toggleInfiniteJump()	infiniteJumpEnabled = not infiniteJumpEnable
 		end)	else
 		jumpButton.Text ="Infinite Jump: OFF"		player.Character.Humanoid.JumpPower = 50 -- Возвращаем силу прыжка к норме, если нужно
 	end
-end
-
--- Привязываем функцию к нажатию кнопки
-jumpButton.MouseButton1Click:Connect(toggleInfiniteJump)
 end
